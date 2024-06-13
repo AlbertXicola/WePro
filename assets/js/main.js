c6604d9223a666cd26d05b6da3f72529e7f34b88
Kichola <131254158+AlbertXicola@users.noreply.github.com>
2024-06-13 20:48:49 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+navToggle.addEventListener('click', () =>{
+   navMenu.classList.add('show-menu')
+})
+
+/* Menu hidden */
+navClose.addEventListener('click', () =>{
+   navMenu.classList.remove('show-menu')
+})
+
+/*=============== SEARCH ===============*/
+const search = document.getElementById('search'),
+      searchBtn = document.getElementById('search-btn'),
+      searchClose = document.getElementById('search-close')
+
+/* Search show */
+searchBtn.addEventListener('click', () =>{
+   search.classList.add('show-search')
+})
+
+/* Search hidden */
+searchClose.addEventListener('click', () =>{
+   search.classList.remove('show-search')
+})
+
+/*=============== LOGIN ===============*/
+const login = document.getElementById('login'),
+      loginBtn = document.getElementById('login-btn'),
+      loginClose = document.getElementById('login-close')
+
+/* Login show */
+loginBtn.addEventListener('click', () =>{
+   login.classList.add('show-login')
+})
+
+/* Login hidden */
+loginClose.addEventListener('click', () =>{
+   login.classList.remove('show-login')
+})
+
+
+document.addEventListener('scroll', function() {
+   const elements = document.querySelectorAll('.main__title, .main__subtitle');
+
+   elements.forEach(element => {
+       const scrollThreshold = parseInt(element.getAttribute('data-scroll'), 10);
+       const scrollTop = window.scrollY;
+
+       if (scrollTop > scrollThreshold) {
+           element.classList.add('visible');
+       }
+   });
+});
+
+
+
+
+document.getElementById('scrollTocapa2').addEventListener('click', function() {
+   document.getElementById('capa2').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('scrollTocapa3').addEventListener('click', function() {
+   document.getElementById('capa3').scrollIntoView({ behavior: 'smooth' });
+});
+
+
+document.getElementById('scrollTocapa1').addEventListener('click', function() {
+   document.getElementById('capa1').scrollIntoView({ behavior: 'smooth' });
+});
+
